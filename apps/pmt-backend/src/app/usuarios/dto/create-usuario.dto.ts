676d4d02cede724c9a5d764f0914ed1302864ccc
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-10-20 19:24:28 -0300

    feat: instalação do swagger no backend

--- apps/pmt-backend/src/app/usuarios/dto/create-usuario.dto.ts
+++ apps/pmt-backend/src/app/usuarios/dto/create-usuario.dto.ts
@@ -1,9 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUsuarioDto {
+  @ApiProperty()
   nome: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty({ required: false })
   telefone?: string;
+
+  @ApiProperty({ required: false })
   endereco?: string;
+
+  @ApiProperty({ enum: ['empresa', 'individual'] })
   tipo: 'empresa' | 'individual';
+
+  @ApiProperty({ required: false })
   avaliacaoMedia?: number;
+
+  @ApiProperty({ required: false })
   foto?: string;
 }
--- apps/pmt-backend/src/app/usuarios/usuarios.controller.ts
+++ apps/pmt-backend/src/app/usuarios/usuarios.controller.ts
@@ -2,32 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
+import { Usuario } from '@prisma/client';
 
+@ApiTags('usuarios') // Adiciona uma tag ao Swagger
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) { }
 
   @Post()
+  @ApiResponse({ status: 201, description: 'Usuário criado.' })
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuariosService.create(createUsuarioDto);
   }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Lista de usuários.' })
   findAll() {
     return this.usuariosService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'Usuário encontrado.' })
   findOne(@Param('id') id: string) {
     return this.usuariosService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiResponse({ status: 200, description: 'Usuário atualizado.' })
   update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     return this.usuariosService.update(id, updateUsuarioDto);
   }
 
   @Delete(':id')
+  @ApiResponse({ status: 200, description: 'Usuário removido.' })
   remove(@Param('id') id: string) {
     return this.usuariosService.remove(id);
   }
